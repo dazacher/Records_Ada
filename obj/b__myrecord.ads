97fc6d88476480a00bcb773bf12da917a14c50b6
dazacher <dazacher2000@yahoo.com>
2021-07-20 06:29:08 -0700

    Nested records.

--- obj/b__myrecord.ads
+++ obj/b__myrecord.ads
@@ -40,7 +40,7 @@ package ada_main is
    pragma Export (C, main, "main");
 
    type Version_32 is mod 2 ** 32;
-   u00001 : constant Version_32 := 16#21c049e4#;
+   u00001 : constant Version_32 := 16#5eace01d#;
    pragma Export (C, u00001, "myrecordB");
    u00002 : constant Version_32 := 16#2e11c0b1#;
    pragma Export (C, u00002, "system__standard_libraryB");
--- src/myrecord.adb
+++ src/myrecord.adb
@@ -11,8 +11,8 @@ procedure Myrecord is
       Point_Count  : Integer range 0 .. 40  := 5;
       Quick_Tricks : Float range 0.0 .. 8.0 := 1.0;
       Bid_Suit     : Suit                   := Pass;
-      --  Bid_Tricks   : Trick_Range            := 0;
-      Bid_Tricks   : Trick_Range            ;
+      Bid_Tricks   : Trick_Range            := 0;
+      --  Bid_Tricks   : Trick_Range            ;
    end record;
 
    --  North : Hand;
@@ -22,6 +22,24 @@ procedure Myrecord is
    --  South : Hand := (Quick_Tricks => 1.5, Bid_Suit => Hearts, Others => 0);
    South : Hand := (others => <>);
    East  : Hand := (Quick_Tricks => 1.5, Bid_Suit => Spades, Bid_Tricks | Point_Count => 0);
+
+   type Result is record
+      Score   : Integer range 0 .. 8250 := 10;
+      Did_Win : Boolean                 := False;
+   end record;
+
+   type Hand_Over is record
+      Hand_Bid    : Hand;
+      Hand_Result : Result := (20, True);
+   end record;
+
+   Final_N : Hand_Over :=
+     (Hand_Result => (0, True), Hand_Bid => (14, 1.5, Clubs, 0));
+   Final_S : Hand_Over;
+   Final_E : Hand_Over := ((14, 1.5, Clubs, 0), (0, Did_Win => <>));
+   Final_W : Hand_Over;
+   Win_Field : Boolean renames Final_N.Hand_Result.Did_Win;
+
 begin
    --  North.Point_Count := 14;
    --  North.Quick_Tricks := 1.5;
@@ -29,6 +47,10 @@ begin
    --  North.Bid_Tricks := 2;
    --
    --  TIO.Put_Line(Integer'Image(North.Point_Count));
+   Final_N.Hand_Bid            := (16, 2.0, Diamonds, Bid_Tricks => 3);
+   Final_N.Hand_Result.Score   := 1_000;
+   Final_W                     := Final_N;
+   Final_W.Hand_Result.Did_Win := False;
 
    TIO.Put_Line (Suit'Image (North.Bid_Suit));
    TIO.Put_Line (Suit'Image (West.Bid_Suit));
@@ -41,4 +63,24 @@ begin
    TIO.Put_Line (Float'Image (South.Quick_Tricks));
    TIO.Put_Line (Suit'Image (South.Bid_Suit));
    TIO.Put_Line (Integer'Image (South.Bid_Tricks));
+
+   TIO.Put_Line (Integer'Image (Final_N.Hand_Bid.Point_Count));
+   TIO.Put_Line (Float'Image (Final_N.Hand_Bid.Quick_Tricks));
+   TIO.Put_Line (Suit'Image (Final_N.Hand_Bid.Bid_Suit));
+   TIO.Put_Line (Integer'Image (Final_N.Hand_Bid.Bid_Tricks));
+   TIO.Put_Line (Integer'Image (Final_N.Hand_Result.Score));
+   TIO.Put_Line (Boolean'Image (Win_Field));
+
+   TIO.Put ("Final_S:");
+   TIO.Put_Line (Boolean'Image (Final_S.Hand_Result.Did_Win));
+   TIO.Put_Line (Integer'Image (Final_S.Hand_Result.Score));
+   TIO.Put ("Final_E:");
+   TIO.Put_Line (Boolean'Image (Final_E.Hand_Result.Did_Win));
+   TIO.Put_Line ("Final_W:");
+   TIO.Put_Line (Integer'Image (Final_W.Hand_Bid.Point_Count));
+   TIO.Put_Line (Float'Image (Final_W.Hand_Bid.Quick_Tricks));
+   TIO.Put_Line (Suit'Image (Final_W.Hand_Bid.Bid_Suit));
+   TIO.Put_Line (Integer'Image (Final_W.Hand_Bid.Bid_Tricks));
+   TIO.Put_Line (Integer'Image (Final_W.Hand_Result.Score));
+   TIO.Put_Line (Boolean'Image (Final_W.Hand_Result.Did_Win));
 end Myrecord;
